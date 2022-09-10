8ad4fb0cf786320abac573ff940b75d9ae64103d
vxpeprivate <108173427+vxpeprivate@users.noreply.github.com>
2022-09-10 10:18:33 -0700

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
-        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/8pmX8/rektsky4roblox/main/mainscript.lua'))()")
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/vxpeprivate/rektsky4roblox/main/mainscript.lua'))()")
     end
 end)
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
@@ -12,5 +12,5 @@ elseif game:HttpGet("https://raw.githubusercontent.com/8pmX8/rektsky4roblox/main
 elseif isfile("rektsky/scripts/"..game.PlaceId..".lua") then
     loadstring(readfile("rektsky/scripts/"..game.PlaceId..".lua"))()
 else    
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/8pmX8/rektsky4roblox/main/scripts/AnyGame.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/8pmX8/vxpeprivate/main/scripts/AnyGame.lua"))()
 end
